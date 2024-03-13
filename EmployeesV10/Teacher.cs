1dca752358505d3c46c8a05e715b8f5d08c0f8d7
Kevin Waweru <kew001@edu.zealand.dk>
2024-03-13 10:44:20 +0100

    Add project files.

---
+++ EmployeesV10/Teacher.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeesV10
+{
+    public class Teacher : Employee
+    {
+        #region Instance fields
+        private int _payGrade;
+        #endregion
+
+        #region Constructor
+        public Teacher(string name, int hoursPerWeek, int payGrade)
+            : base(name, hoursPerWeek)
+        {
+            _payGrade = payGrade;
+        }
+        #endregion
+
+        #region Properties
+        public int PayGrade
+        {
+            get { return _payGrade; }
+            set { _payGrade = value; }
+        }
+
+        public string AllInformation
+        {
+            get
+            {
+                return $"Teacher {Name} works {HoursPerWeek} hours/week, at paygrade {PayGrade}";
+            }
+        } 
+        #endregion
+    }
+}
